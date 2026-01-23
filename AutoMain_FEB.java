1ed1442f21e3b233cd33987cf375baed4024b058
Dylan <jvo78010@gmail.com>
2026-01-22 22:45:20 -0500

    Implement webcam and intake control in AutoMain_FEB
    Added webcam functionality and intake control methods.

--- AutoMain_FEB.java
+++ AutoMain_FEB.java
@@ -6,6 +6,8 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 //import com.qualcomm.robotcore.hardware.CRServo;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+
 
 @Autonomous(name = "Auto January 2026 - Red")
 
@@ -17,7 +19,8 @@ public class AutoMain_FEB extends LinearOpMode {
     private DcMotor fRubberWheel; // Front Rubber Band Intake Wheel
     private DcMotor bRubberWheel; // Back Rubber Band Intake Wheel
     private DcMotor launch; // Launch Rubber Wheel set with Gearbox
-    
+    private OpenCvWebcam webcam; //Hopely Webcam but we need to check with Aarit 
+
     private ElapsedTime clock = new ElapsedTime();
 
     @Override
@@ -29,6 +32,7 @@ public class AutoMain_FEB extends LinearOpMode {
         fRubberWheel = hardwareMap.get(DcMotor.class, "front wheel");
         bRubberWheel = hardwareMap.get(DcMotor.class, "back wheel");
         launch = hardwareMap.get(DcMotor.class, "launch");
+        webcam = hardwareMap.get(DcMotor.class,"Webcam 1"); //Need to change the class it comes from but I don't know which 
 
         // We set these based on how the motors are physically installed
         backrightmotor.setDirection(DcMotor.Direction.REVERSE);
@@ -59,6 +63,24 @@ public class AutoMain_FEB extends LinearOpMode {
             stopMotors();
             
             clock.reset();
+            startIntakes();
+            moveForward();
+            while(clock.seconds()<=1.5){
+            }
+            stopMotors();
+            stopIntakes();
+            clock.reset();
+            turnRight();
+            while(clock.second()<=1){
+            }
+            stopMotors();
+            clock.reset();
+            moveForward();
+            while(clock.seconds()<=3.5){ //idk how long to make it so, I just did this for now 
+            }
+            stopMotors();
+            clock.reset();
+
 
         }
         
@@ -114,6 +136,20 @@ public class AutoMain_FEB extends LinearOpMode {
         frontrightmotor.setPower(0);
 
     }
+
+     public void startIntakes()
+    {
+        fRubberWheel.setPower(0.5);
+        bRubberWheel.setPower(0.5);
+    }
+
+    public void stopIntakes()
+    {
+        fRubberWheel.setPower(0);
+        bRubberWheel.setPower(0);
+    }
+    
     
 }
+
 
